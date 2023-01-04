d0cd67a055d4515f71d6a44a26872d716a5115e6
tarek koussaier <103568623+Tarekkous@users.noreply.github.com>
2023-01-04 16:11:55 +0100

    Update db.js

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const pool = new Pool ({
     user : 'postgres_sql',
     password: 'LCcOCmJQrgFIMQVNQG3RNzQTgtcg7kkb',
     host: 'dpg-ceql1d9gp3jv6g755vag-a.frankfurt-postgres.render.com',
-    localhost: 5432,
+    port: 5432,
     database: 'travel_explore_db'
 });
 
