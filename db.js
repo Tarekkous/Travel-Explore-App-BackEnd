adb92a495da7b0c85ae981655b7cb733af62f0c9
tarek koussaier <103568623+Tarekkous@users.noreply.github.com>
2023-01-04 15:49:50 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool ({
-    user : 'postgres',
-    password: 'database',
-    host: 'localhost',
+    user : 'postgres_sql',
+    password: 'LCcOCmJQrgFIMQVNQG3RNzQTgtcg7kkb',
+    host: 'dpg-ceql1d9gp3jv6g755vag-a.frankfurt-postgres.render.com',
     localhost: 5432,
-    database: 'TRAVEL&EXPLORE-DB'
+    database: 'travel_explore-db'
 });
 
 module.exports = pool;
