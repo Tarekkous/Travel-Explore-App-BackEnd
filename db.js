dd44e4438fc9a181b47d5a91275d7f3d35ce712d
tarek koussaier <103568623+Tarekkous@users.noreply.github.com>
2023-01-04 15:50:57 +0100

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const Pool = require('pg').Pool;
 const pool = new Pool ({
     user : 'postgres_sql',
     password: 'LCcOCmJQrgFIMQVNQG3RNzQTgtcg7kkb',
-    host: 'dpg-ceql1d9gp3jv6g755vag-a.frankfurt-postgres.render.com',
+    host: 'https://travelandexplore-app-back-end.onrender.com',
     localhost: 5432,
     database: 'travel_explore-db'
 });
